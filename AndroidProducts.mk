90ddaa8c4ecb5e76a8c3a68b9c169d514fac9bc6
Marshmellow <marshmellow@example.com>
2026-04-15 07:04:56 -0500

    yes yes

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,9 @@
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_eureka.mk
-COMMON_LUNCH_CHOICES := twrp_eureka-eng
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_eureka.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_eureka-eng
+PRODUCT_SOONG_NAMESPACES += system/extras system/core bootable/recovery
+PRODUCT_PACKAGES += libfec libfec_rs
+PRODUCT_BUILD_SYSTEM_IMAGE := false
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
--- twrp_eureka.mk
+++ twrp_eureka.mk
@@ -1,3 +1,4 @@
+TW_THEME := landscape_hdpi
 PRODUCT_RELEASE_NAME := eureka
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
@@ -9,5 +10,12 @@ PRODUCT_MODEL := Quest 3
 PRODUCT_MANUFACTURER := Oculus
 
 PRODUCT_GMS_CLIENTID_BASE := android-oculus
+
+PRODUCT_IS_RECOVERY := true
+PRODUCT_BUILD_SYSTEM_IMAGE := false
+PRODUCT_BUILD_RAMDISK_IMAGE := true
+
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+PRODUCT_ENFORCE_VINTF_MANIFEST := false
 
 $(call inherit-product, vendor/twrp/config/common.mk)
